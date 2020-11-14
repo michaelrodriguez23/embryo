56a58bff0df208e537f2ab773e6cc849060aa647
Michael Rodriguez <michaelrodriguez23@gmail.com>
2020-11-14 13:26:58 -0500

    test

--- sketch.js
+++ sketch.js
@@ -1,29 +1,27 @@
 var song;
 var slider
-
 var sliderRate
 var sliderPan;
-
 var button;
 var jumpButton;
-
-function preload() {
-  song = loadSound("Audio/sadeFlip.wav", loaded);
-  song.setVolume(0.9);
-
-}
 
 function setup() {
   createCanvas(1200, 1200, WEBGL);
   tint(0, 153, 204, 126);
-  embryo = loadImage('Embryo.jpg');
-  embryo2 = loadImage('embryo3.jpg');
-  embryo3 = loadImage('Embryo-2.gif');
-  mitosis = loadImage('mitosis.jpg');
-  dna = loadImage('dna.gif');
-  // cam = createCapture(VIDEO);
-  amp = new p5.Amplitude();
   background(51, 20, 20);
+  amp = new p5.Amplitude();
+}
+function preload() {
+  song = loadSound("audio/sadeFlip.wav", imageLoad);
+  song.setVolume(0.9);
+}
+function imageLoad(){
+  embryo = loadImage('assets/Embryo.jpg');
+  embryo2 = loadImage('assets/embryo3.jpg');
+  embryo3 = loadImage('assets/Embryo-2.gif');
+  mitosis = loadImage('assets/mitosis.jpg');
+  dna = loadImage('assets/dna.gif');
+
 }
 
 function draw() {
@@ -32,12 +30,9 @@ function draw() {
   var vol = amp.getLevel();
   var diam = map(vol, 0, 0.3, 80, 500);
   fill(20);
-
   if (vol > .010) {
-
     fill(255);
     texture(embryo3);
-
   }
   if (vol > .030) {
     fill(255);
@@ -46,16 +41,12 @@ function draw() {
   if (vol > .040) {
     fill(40);
     texture(embryo2);
-
   }
   if (vol > .050) {
     ambientLight(255, 50, 0);
     ambientMaterial(250);
-
-
     fill(250);
     texture(mitosis);
-
   }
   if (vol > .060) {
     fill(250);
@@ -68,23 +59,17 @@ function draw() {
     texture(embryo);
     background(255);
   }
-
-
   sphere(diam);
   var x = 1;
 }
 
-function loaded() {
 
-}
 
 function mouseClicked() {
   togglePlaying();
 }
 
 function togglePlaying() {
-
-
   if (!song.isPlaying()) {
     song.play();
     song.setVolume(0.3);
@@ -93,11 +78,4 @@ function togglePlaying() {
     song.pause();
 
   }
-}
-
-function jumpSong() {
-  var len = song.duration(); // puts song length in var len
-  var t = random(len);
-  var ct = song.currentTime();
-  song.jump(t);
 }
