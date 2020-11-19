7611fc9f88fe781b21572e51e33b753f5ee19367
Michael Rodriguez <michaelrodriguez23@gmail.com>
2020-11-18 20:28:20 -0500

    audio update

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ let bg;
 let y = 0;
 
 function preload() {
-  song = loadSound("SPACE.WAV");
+  song = loadSound("audio/SPACE.WAV");
   song.setVolume(0.9);
   embryo = loadImage('assets/Embryo.jpg');
   embryo2 = loadImage('assets/embryo3.jpg');
